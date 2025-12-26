03e1ae4e6728bd3feed617d40b26c26bec84e36e
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2025-12-26 21:17:21 +0500

    Commited

--- loader.py
+++ loader.py
@@ -1,26 +1,23 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from data import config
 from utils.db_api.admins import AdminsDB
 from utils.db_api.create_tables import Database
 from utils.db_api.lessons import LessonsDB
-from utils.db_api.paid_lessons import PaidLessonsDB
 from utils.db_api.users import UsersDB
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-# storage = RedisStorage2(
-#     host='localhost',
-#     port=6379,
-#     db=5,
-#     state_ttl=3600,
-#     data_ttl=3600,
-#     password=REDIS_PASS
-# )
-storage = MemoryStorage()
+storage = RedisStorage2(
+    host='localhost',
+    port=6379,
+    db=5,
+    state_ttl=3600,
+    data_ttl=3600,
+    password=REDIS_PASS
+)
+# storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 db = Database()
 udb = UsersDB(db)
 admdb = AdminsDB(db)
-pdb = PaidLessonsDB(db)
 lesdb = LessonsDB(db)
